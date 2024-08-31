dd96c5c0a1a458bd4d7c1f8d0863735736518f38
Jeetin9410 <kumaranand8126@gmail.com>
2024-08-31 23:37:32 +0530

    feat: Card ui with theme and text style

--- lib/main.dart
+++ lib/main.dart
@@ -45,10 +45,10 @@ class MyHomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(height: 20),
-            Text('A random idea:'),
+            SizedBox(height: 10),
             //Text(appState.current.asLowerCase), // not got extra info passed to Text
             BigCard(pair: pair), // better appraoch to give only required data
+            SizedBox(height: 10),
             ElevatedButton(onPressed: (){
               appState.getNext();
               }, child: Text("Click Me!"))
@@ -71,10 +71,21 @@ class BigCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    final style = theme.textTheme.displaySmall!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
     return Card(
+      color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Text(pair.asLowerCase),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
