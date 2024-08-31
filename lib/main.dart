a696e463cdd22fba9b13482c2737e4cf9a7c5381
Jeetin9410 <kumaranand8126@gmail.com>
2024-08-31 22:38:32 +0530

    feat: adding button functionality

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,11 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -42,6 +47,7 @@ class MyHomePage extends StatelessWidget {
           Text('A random idea:'),
           Text(appState.current.asLowerCase),
           ElevatedButton(onPressed: (){
+            appState.getNext();
             }, child: Text("Click Me!"))
         ],
       ),
